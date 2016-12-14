205f83c51c0b8856efb587f3e9e7f54b48abd9a8
Clasyc <d.dewux@gmail.com>
2016-12-14 18:12:31 +0200

    Remanaged readers subsystem directories and controllers

---
+++ src/AppBundle/Controller/ReadersSubsystem/PenaltyController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller\ReadersSubsystem;
+
+use AppBundle\Entity\Wishlists;
+use AppBundle\Form\Type\ReaderRegistration_NoFosType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class PenaltyController extends Controller
+{
+
+
+}
